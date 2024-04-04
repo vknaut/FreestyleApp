401242ebcc1259a9964e5109710084f76189562e
vknaut <victor.louis.knaut@gmail.com>
2024-04-04 15:56:04 +0200

    Changed formatting of colorDict

--- FreestyleApp/utils/utils.py
+++ FreestyleApp/utils/utils.py
@@ -1,28 +1,28 @@
 colorDict = {
-# Warm
-"RED" : ( 188 , 0 , 0 ),
-"YELLOW" : ( 204 , 205 , 0 ),
-# Oranges
-"ORANGE" : ( 250 , 88 , 0 ),
-"LIGHTER_ORANGE" : ( 255 , 153 , 51 ),
-"PEACH" : ( 255 , 229 , 204 ),
-# Greens
-"DARK_GREEN" : ( 0 , 100 , 100 ),
-"DARK_GREEN2" : ( 0 , 102 , 0 ),
-"GREEN_BLUE" : ( 0 , 75 , 100 ),
-# Blues
-"MARINE" : ( 0 , 0 , 153 ),
-"LIGHT_BLUE" : ( 0 , 128 , 255 ),
-"DARKER_BLUE" : ( 0 , 50 , 100 ),
-# Various
-"SAND" : ( 153 , 150 , 30 ),
-"GREY" : ( 180 , 180 , 180 ),
-"DARKER_GREY" : ( 128 , 128 , 128 ),
-"BLACK" : ( 0 , 0 , 0 ),
-"WHITE" : ( 240 , 240 , 240 ),
-"PINK" : ( 200 , 0 , 255 ),
-"LACHS" : (180 , 100 , 100 ),
-"BROWN" : (100 , 50, 0)
+    # Warm
+    "RED" : ( 188 , 0 , 0 ),
+    "YELLOW" : ( 204 , 205 , 0 ),
+    # Oranges
+    "ORANGE" : ( 250 , 88 , 0 ),
+    "LIGHTER_ORANGE" : ( 255 , 153 , 51 ),
+    "PEACH" : ( 255 , 229 , 204 ),
+    # Greens
+    "DARK_GREEN" : ( 0 , 100 , 100 ),
+    "DARK_GREEN2" : ( 0 , 102 , 0 ),
+    "GREEN_BLUE" : ( 0 , 75 , 100 ),
+    # Blues
+    "MARINE" : ( 0 , 0 , 153 ),
+    "LIGHT_BLUE" : ( 0 , 128 , 255 ),
+    "DARKER_BLUE" : ( 0 , 50 , 100 ),
+    # Various
+    "SAND" : ( 153 , 150 , 30 ),
+    "GREY" : ( 180 , 180 , 180 ),
+    "DARKER_GREY" : ( 128 , 128 , 128 ),
+    "BLACK" : ( 0 , 0 , 0 ),
+    "WHITE" : ( 240 , 240 , 240 ),
+    "PINK" : ( 200 , 0 , 255 ),
+    "LACHS" : (180 , 100 , 100 ),
+    "BROWN" : (100 , 50, 0)
 }
 
  
